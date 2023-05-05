977d77ff0051a2052784498613fba2ffd66cf0ec
Louie Cai <louiecai3@gmail.com>
2023-05-04 23:29:56 -0700

    question class created

---
+++ src/main/java/org/agilelovers/ui/object/Question.java
@@ -0,0 +1,42 @@
+package org.agilelovers.ui.object;
+
+public class Question {
+    private String question = "";
+    private String answer = "";
+    private String title = "";
+
+    private Question() {
+
+    }
+
+    public Question(String title, String question, String answer) {
+        this.title = title;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String question() {
+        return this.question;
+    }
+
+    public String answer() {
+        return this.answer;
+    }
+
+    public void setAnswer(String newAnswer) {
+        this.answer = newAnswer;
+    }
+
+    public void setQuestion(String newQuestion) {
+        this.question = newQuestion;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
+}
