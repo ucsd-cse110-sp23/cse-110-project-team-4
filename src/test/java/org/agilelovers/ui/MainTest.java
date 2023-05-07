0e5a79d7cc63ac0561e48a35bb34929af6d6e02a
lamnn <nicholasnlamb@gmail.com>
2023-05-07 07:32:00 -0700

    added test files for each class - test files are trivial or not implemented yet updated pom.xml file to include additional testing dependencies

---
+++ src/test/java/org/agilelovers/ui/MainTest.java
@@ -0,0 +1,13 @@
+package org.agilelovers.ui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        Main.main(new String[] {});
+        Assertions.assertTrue(true);
+    }
+}
