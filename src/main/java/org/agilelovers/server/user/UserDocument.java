3f0e0074fd1fa4d333ce6e2211b029ded1f6a79c
Anish Govind <anishg24@gmail.com>
2023-05-27 17:23:34 -0700

    Updated tests. Need to add more tests

--- src/main/java/org/agilelovers/server/user/UserDocument.java
+++ src/main/java/org/agilelovers/server/user/UserDocument.java
@@ -1,5 +1,8 @@
 package org.agilelovers.server.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,58 +13,23 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Document("sayit-users")
+@Data
+@Builder
+@AllArgsConstructor
 public class UserDocument {
     @Id
     private String id;
+
     @NotNull
     @NotBlank
     @Email
     @Indexed(name = "username", unique = true)
     private String username;
+
     @Email
     private String email;
+
     @NotNull
     @NotBlank
     private String password;
-
-    public UserDocument(String id, String username, String email,
-                        String password) {
-        super();
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/org/agilelovers/server/UserControllerTest.java
+++ src/test/java/org/agilelovers/server/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.agilelovers.server.user.UserRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.opentest4j.TestAbortedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,35 +46,57 @@ public class UserControllerTest {
         userRepository.deleteAll();
     }
 
-//    @Test
-//    public void createUser() throws Exception {
-//        UserDocument user = new UserDocument("test", "testing@test.com",
-//                "plaintext");
-//
-//        mvc.perform(post("/api/users")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(JsonUtil.toJson(user))
-//        );
-//
-//        List<UserDocument> found = userRepository.findAll();
-//        assertThat(found).extracting(UserDocument::getUsername).containsOnly(
-//                "test");
-//    }
-//
-//    @Test
-//    public void createUserAndChangeEmail() throws Exception {
-//        UserDocument user = new UserDocument("indecisive_user", "maybe@test" +
-//                ".com", "plaintext");
-//
-//        mvc.perform(post("/api/users")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(JsonUtil.toJson(user))
-//        );
-//
-//        List<UserDocument> found = userRepository.findAll();
-//        assertThat(found).extracting(UserDocument::getUsername).containsOnly(
-//                "indecisive_user");
-//
-//    }
+    @Test
+    public void createUser() throws Exception {
+        UserDocument user = UserDocument.builder()
+                .username("testing@test.com")
+                .password("plaintext")
+                .build();
 
+        mvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.toJson(user))
+        );
+
+        List<UserDocument> found = userRepository.findAll();
+        assertThat(found).extracting(UserDocument::getUsername).containsOnly(
+                "testing@test.com");
+    }
+
+    @Test
+    public void createUserAndChangeEmail() throws Exception {
+
+        String username = "unsure_testing@test.com";
+        String email = "now_sure_testing@test.com";
+        String password = "plaintext";
+
+        UserDocument user = UserDocument.builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        mvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.toJson(user))
+        );
+
+        UserDocument found = userRepository.findByUsernameAndPassword(username, password)
+                .orElseThrow(TestAbortedException::new);
+
+        assertThat(found).extracting(UserDocument::getUsername).isEqualTo(username);
+        assertThat(found).extracting(UserDocument::getEmail).isNull();
+
+        String id = found.getId();
+
+        mvc.perform(put("/api/users/" + id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(email)
+        );
+
+        found = userRepository.findById(id)
+                .orElseThrow(TestAbortedException::new);
+
+        assertThat(found).extracting(UserDocument::getUsername).isEqualTo(username);
+        assertThat(found).extracting(UserDocument::getEmail).isEqualTo(email);
+    }
 }
